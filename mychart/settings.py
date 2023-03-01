3b3348bfbc161f4ea04e7e275ba74d30fea4514d
Anshuman Rout <routanshuman01@gmail.com>
2023-03-01 15:21:03 +0530

    6th

--- mychart/settings.py
+++ mychart/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
+    'whitenoise.runserver_nostatic', 
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'mychart.urls'
@@ -121,6 +123,7 @@ STATICFILES_DIRS = [
     BASE_DIR/'static',
 ]
 STATIC_ROOT = BASE_DIR/'staticfiles'
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
