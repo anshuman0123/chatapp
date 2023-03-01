927429e37cc873d002f5f5f4060b78cdccc071d3
Anshuman Rout <routanshuman01@gmail.com>
2023-03-01 14:58:44 +0530

    5th

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn mychat.wsgi
+web: gunicorn mychart.wsgi
--- mychart/wsgi.py
+++ mychart/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mychat.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mychart.settings')
 
 application = get_wsgi_application()
